3e841e36bc7b0f6baaa3aafe8dea3b1ec86318fc
bernie-guo <1456252990@qq.com>
2021-03-30 19:46:51 +0800

    2020.3.30

--- src/main/java/com/nwu/service/CustomizeService.java
+++ src/main/java/com/nwu/service/CustomizeService.java
@@ -5,8 +5,43 @@ package com.nwu.service;
  * @time 2021.03.22
  */
 
+import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinition;
+import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinitionList;
+import io.fabric8.kubernetes.api.model.batch.CronJob;
+
+import java.io.FileNotFoundException;
+
+
 /**
  * 自定义资源的 service 层接口
  */
 public interface CustomizeService {
+
+    /**
+     * 获取CustomResourceDefinition列表
+
+     * @return CustomResourceDefinition列表
+     */
+    CustomResourceDefinitionList getCustomResourceDefinition() ;
+
+    /**
+     * 删除CustomResourceDefinition
+     * @param customResourceDefinition 要删除的自定义资源
+     * @return CustomResourceDefinition列表
+     */
+    boolean  deleteCustomResourceDefinition(CustomResourceDefinition customResourceDefinition);
+
+    /**
+     * 加载以一个CustomResourceDefinition
+     * @param path 加载自定义资源的yaml文件路径
+     * @return CustomResourceDefinition列表
+     */
+    CustomResourceDefinition loadCustomResourceDefinition(String path)throws FileNotFoundException;
+
+    /**
+     * 创建以一个CustomResourceDefinition
+     * @param path 加载自定义资源的yaml文件路径
+     * @return CustomResourceDefinition列表
+     */
+    CustomResourceDefinition createCustomResourceDefinition(String path)throws FileNotFoundException;
 }
--- src/main/java/com/nwu/service/impl/CustomizeServiceImpl.java
+++ src/main/java/com/nwu/service/impl/CustomizeServiceImpl.java
@@ -1,7 +1,16 @@
 package com.nwu.service.impl;
 
 import com.nwu.service.CustomizeService;
+import com.nwu.util.KubernetesUtils;
+import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinition;
+import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinitionList;
 import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import static com.nwu.util.GetYamlInputStream.byPath;
 
 /**
  * @author Rex Joush
@@ -13,4 +22,31 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class CustomizeServiceImpl implements CustomizeService {
+
+
+    public CustomResourceDefinition loadCustomResourceDefinition(String path) throws FileNotFoundException {
+
+        InputStream yamlInputStream = byPath(path);
+        CustomResourceDefinition customResourceDefinition=KubernetesUtils.client.apiextensions().v1().customResourceDefinitions().load(yamlInputStream).get();
+        return customResourceDefinition;
+    }
+
+    @Override
+    public CustomResourceDefinition createCustomResourceDefinition(String path) throws FileNotFoundException {
+
+        CustomResourceDefinition customResourceDefinition=KubernetesUtils.client.apiextensions().v1().customResourceDefinitions().createOrReplace(loadCustomResourceDefinition(path));
+        return customResourceDefinition;
+    }
+
+    @Override
+    public CustomResourceDefinitionList getCustomResourceDefinition() {
+        CustomResourceDefinitionList crdList= KubernetesUtils.client.apiextensions().v1().customResourceDefinitions().list();
+        return crdList;
+    }
+
+    @Override
+    public boolean deleteCustomResourceDefinition(CustomResourceDefinition customResourceDefinition) {
+        boolean deleted= KubernetesUtils.client.apiextensions().v1().customResourceDefinitions().delete(customResourceDefinition);
+        return deleted;
+    }
 }
