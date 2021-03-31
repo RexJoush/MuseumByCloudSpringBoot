eff2ce002c9a27af9a01e1adea71e99358e77b73
Anna667 <[632177309@qq.com]>
2021-03-31 17:39:56 +0800

    Anna667

--- src/test/java/com/nwu/MuseumByCloudApplicationTests.java
+++ src/test/java/com/nwu/MuseumByCloudApplicationTests.java
@@ -2,7 +2,6 @@ package com.nwu;
 
 import com.nwu.dao.UserDao;
 import com.nwu.entity.User;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
@@ -13,7 +12,7 @@ class MuseumByCloudApplicationTests {
     @Resource
     private UserDao userDao;
 
-    @Test
+
     void contextLoads() {
         User admin = userDao.findByUsername("admin");
         System.out.println(admin);
