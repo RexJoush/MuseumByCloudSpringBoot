b530b1fe8ff0765d793010a76dbee7c8468f6d67
Joush <37282701+RexJoush@users.noreply.github.com>
2021-04-09 11:40:48 +0800

    2021.04.09 Pod Edit Complete By R.Joush

--- src/main/java/com/nwu/service/CommonService.java
+++ src/main/java/com/nwu/service/CommonService.java
@@ -5,6 +5,8 @@ package com.nwu.service;
  * @time 2021.04.07
  */
 
+import java.io.File;
+
 /**
  * 公有方法 service 层接口
  * 修改资源，删除资源等等
@@ -15,7 +17,10 @@ public interface CommonService {
      * 根据传入的 yaml 文件进行资源的修改
      * @param yaml yaml 文件字符串
      * @return 创建结果信息
+     *          1200 表示成功
+     *          1201 表示创建失败
+     *          1202 表示文件有问题
      */
-    String changeResourceByYaml(String yaml);
+    int changeResourceByYaml(File yaml);
 
 }
--- src/main/java/com/nwu/util/KubernetesUtils.java
+++ src/main/java/com/nwu/util/KubernetesUtils.java
@@ -16,6 +16,7 @@ import io.fabric8.kubernetes.client.KubernetesClient;
 public class KubernetesUtils {
 
     private static Config config;
+
     static {
         // kubernetes 连接 token
         String token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IlhOc2N4Q000WEZoZHJGanNfWkg1UXNhRkJfNHFESk1JenB5T1ptNnBUMTAifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJrdWJlcm5ldGVzLWRhc2hib2FyZCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJhZG1pbi11c2VyLXRva2VuLWJja3NwIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6ImFkbWluLXVzZXIiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC51aWQiOiJkYjE2NTMyZi00OWIxLTQ4ZDEtYWI5Ni1kOGM4NWI1MGFhMWUiLCJzdWIiOiJzeXN0ZW06c2VydmljZWFjY291bnQ6a3ViZXJuZXRlcy1kYXNoYm9hcmQ6YWRtaW4tdXNlciJ9.rJ6HLq7BCFGn-aZ6lu4CYqVKksPHJSwwI04Hnlt8yhM3w0ReOCRp4HD-K1zkfih7hcDedGcPVQyjS9B57YCHJkgIAcYO3UghiHo-hyOycbPpBukxpiuao5LuZJKthMlVrgf0wD9utiYBUek_ngB0mJNX4GrtZ3E5gtBRYFWdfA_9j60zxeEcYcHH4spd5KXFOV0lRUg_McVZPOKsFJCRRWbGbJWkGOAr8TlbAtMEQA03dz6Kw6zCR6al2dfLufXoavIq2RKvg8_eX8I6tHQfiExMC5uafeVBLXTrMYMKzCom1A3kVMIaxG3YqS91gg1uCaqJdRK3U91xECaQjiPGdA";
@@ -28,5 +29,8 @@ public class KubernetesUtils {
 
     // 创建 Kubernetes 客户端
     public static KubernetesClient client = new DefaultKubernetesClient(config);
+
+    // 增删资源的 yaml 文件临时路径
+    public static String path = "D:\\Files\\a.yaml";
 
 }
