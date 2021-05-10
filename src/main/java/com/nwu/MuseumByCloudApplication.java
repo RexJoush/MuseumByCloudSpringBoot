566f28ecd86048767bc27b275159648dd4ac70cd
Joush <37282701+RexJoush@users.noreply.github.com>
2021-05-10 20:46:08 +0800

    2021.05.10 R.Joush

--- src/main/java/com/nwu/MuseumByCloudApplication.java
+++ src/main/java/com/nwu/MuseumByCloudApplication.java
@@ -6,7 +6,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-//@CrossOrigin // 配置跨域支持
+@CrossOrigin // 配置跨域支持
 @EnableAsync(proxyTargetClass = true) // 配置多线程支持
 public class MuseumByCloudApplication {
 
